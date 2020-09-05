87e177f5e0e229b96b755cfdc0bc335bce43524f
anasshami94 <anas.shami94@gmail.com>
2020-09-05 12:30:06 +0300

    add explore task ui

--- public/js/main.dev.js
+++ public/js/main.dev.js
@@ -1,15 +1,44 @@
 "use strict";
 
-var dragStart = function dragStart(event) {
-  console.log('dragged');
-  event.currentTarget.classList.add('dragging');
+var element = null;
+
+var dragStart = function dragStart(ev) {
+  ev.currentTarget.classList.add('dragging');
+  element = ev.target;
 };
 
-var dragEnd = function dragEnd(event) {
-  event.currentTarget.classList.remove('dragging');
+function dragEnter(ev) {
+  ev.preventDefault();
+  return true;
+}
+
+function dragOver(ev) {
+  ev.preventDefault();
+  return false;
+}
+
+var dragEnd = function dragEnd(ev) {
+  element.classList.remove('dragging');
+  element = null;
 };
+
+function dragDrop(ev) {
+  if (!ev.target.classList.contains('tasks')) return false;
+  element.parentNode.removeChild(element);
+  ev.target.appendChild(element);
+  element.classList.remove('dragging');
+}
 
 Array.from(document.querySelectorAll('.card')).forEach(function (card) {
+  card.setAttribute('draggable', true);
   card.addEventListener('dragstart', dragStart);
   card.addEventListener('dragend', dragEnd);
+  Array.from(card.children).forEach(function (child) {
+    child.setAttribute('draggable', false);
+  });
+});
+Array.from(document.querySelectorAll('.tasks')).forEach(function (col) {
+  col.addEventListener('ondrop', dragDrop);
+  col.addEventListener('ondragenter', dragEnter);
+  col.addEventListener('ondragover', dragOver);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,40 @@
-const dragStart = function(event) {
-    console.log('dragged')
-    event.currentTarget.classList.add('dragging');
+var element = null;
+const dragStart = function(ev) {
+    ev.currentTarget.classList.add('dragging');
+    element = ev.target;
 };
 
-const dragEnd = function(event) {
-    event.currentTarget.classList.remove('dragging');
+function dragEnter(ev) {
+    ev.preventDefault();
+    return true;
+}
+function dragOver(ev) {
+    ev.preventDefault();
+    return false;
+}
+const dragEnd = function(ev) {
+    element.classList.remove('dragging');
+    element = null;
 };
+
+function dragDrop(ev) {
+    if(!ev.target.classList.contains('tasks')) return false;
+    element.parentNode.removeChild(element);
+    ev.target.appendChild(element);
+    element.classList.remove('dragging');
+}
 
 Array.from(document.querySelectorAll('.card')).forEach(function(card) {
+    card.setAttribute('draggable', true);
     card.addEventListener('dragstart', dragStart);
     card.addEventListener('dragend', dragEnd);
+    Array.from(card.children).forEach(function(child) {
+        child.setAttribute('draggable',false);
+    });
+});
+
+Array.from(document.querySelectorAll('.tasks')).forEach(function(col) {
+    col.addEventListener('ondrop', dragDrop);
+    col.addEventListener('ondragenter', dragEnter);
+    col.addEventListener('ondragover', dragOver);
 });
